2322b304f31d45df378ad62efa8be1725653378b
JUN <JUN@DESKTOP-7SLETGG>
2021-04-27 14:31:37 +0900

    add project

---
+++ jtoh/src/main/java/com/multi/kdigitalboot2/JtohApplication.java
@@ -0,0 +1,13 @@
+package com.multi.kdigitalboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JtohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JtohApplication.class, args);
+	}
+
+}
